9b7c04b9460f45c564b18e47df2da3e20834fc05
Erwandec <decarne.erwan@gmail.com>
2026-03-31 01:05:03 +0200

    Delete agent-immo directory

--- agent-immo/backend/utils/distance.py
+++
@@ -1,6 +0,0 @@
-import requests, os
-def compute_drive_time(addr):
- api=os.getenv('ORS_API_KEY')
- r=requests.post('https://api.openrouteservice.org/v2/matrix/driving-car',json={'locations':[[addr['lng'],addr['lat']],[2.319,48.817]],'metrics':['duration']},headers={'Authorization':api})
- sec=r.json()['durations'][0][1]
- return sec/60

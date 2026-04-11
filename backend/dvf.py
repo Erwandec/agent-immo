fda2f5e87a63921953f8819673e1954bef4d0aad
Erwandec <decarne.erwan@gmail.com>
2026-04-12 01:19:33 +0200

    Update dvf.py

--- backend/dvf.py
+++ backend/dvf.py
@@ -6,10 +6,11 @@ from datetime import datetime
 
 def haversine(lat1, lon1, lat2, lon2):
   R = 6371000 
-  phi1, phi2 = math.radians(lat1), math.radians(lat2) 
+  phi1 = math.radians(lat1)
+  phi2 = math.radians(lat2) 
   dphi = math.radians(lat2 - lat1) 
   dlambda = math.radians(lon2 - lon1) 
-  a = math.sin(dphi/2)**2 + math.cos(phi1)math.cos(phi2)math.sin(dlambda/2)**2 
+  a = math.sin(dphi/2)**2 + math.cos(phi1)*math.cos(phi2)*math.sin(dlambda/2)**2 
   return 2*R*math.atan2(math.sqrt(a), math.sqrt(1-a)) 
   
 def date_weight(date_str): 
